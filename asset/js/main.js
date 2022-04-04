44622c47c882a4dd0095f6505d42bf8f5c6f1b45
Andrea Pontillo <andreapontillo97@gmail.com>
2022-04-04 15:47:30 +0200

    stesura parziale esercizio

--- asset/js/main.js
+++ asset/js/main.js
@@ -4,7 +4,7 @@ var app = new Vue(
         data: {
             profilo: {
                 name: 'Sofia',
-                img: 'io'
+                img: '_io'
             },
 
             contacts: [
